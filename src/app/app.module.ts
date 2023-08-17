572cd6552ad232807405ab13fd4fa7532ba8e640
simonvillarin <simon.villarin@gmail.com>
2023-08-17 11:51:59 +0800

    folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './modules/landing/pages/landing/landing.component';
+import { AboutComponent } from './modules/landing/components/about/about.component';
+import { ContactComponent } from './modules/landing/components/contact/contact.component';
+import { HeroComponent } from './modules/landing/components/hero/hero.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, LandingComponent, AboutComponent, ContactComponent, HeroComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
