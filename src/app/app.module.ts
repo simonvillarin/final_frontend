4cce38ae9823c12fd3a5aba4ca8a4ff502d443f1
simonvillarin <simon.villarin@gmail.com>
2023-08-17 13:03:34 +0800

    folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './modules/landing/pages/landing/landing.component';
-import { AboutComponent } from './modules/landing/components/about/about.component';
-import { ContactComponent } from './modules/landing/components/contact/contact.component';
-import { HeroComponent } from './modules/landing/components/hero/hero.component';
 
 @NgModule({
-  declarations: [AppComponent, LandingComponent, AboutComponent, ContactComponent, HeroComponent],
+  declarations: [AppComponent, LandingComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
