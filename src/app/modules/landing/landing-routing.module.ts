4656fb6b87e48661832ad4340cc3158a1fc0b014
AdrianRecortos <65625114+adrianrecortos@users.noreply.github.com>
2023-08-17 15:37:51 +0800

    register page

--- src/app/modules/landing/landing-routing.module.ts
+++ src/app/modules/landing/landing-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
   },
 ];
 
--- src/app/modules/landing/landing.module.ts
+++ src/app/modules/landing/landing.module.ts
@@ -5,6 +5,7 @@ import { AboutComponent } from './components/about/about.component';
 import { TestimonialsComponent } from './components/testimonials/testimonials.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LandingRoutingModule } from './landing-routing.module';
+import { RegisterComponent } from './pages/register/register.component';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { LandingRoutingModule } from './landing-routing.module';
     AboutComponent,
     TestimonialsComponent,
     HomeComponent,
+    RegisterComponent,
   ],
   imports: [CommonModule, LandingRoutingModule],
 })
