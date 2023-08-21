daebd31a946e020384a2ba544ff46cad07d14a50
AdrianRecortos <65625114+adrianrecortos@users.noreply.github.com>
2023-08-21 19:19:46 +0800

    admin service

--- src/app/modules/admin/pages/admin-main/admin-main.component.ts
+++ src/app/modules/admin/pages/admin-main/admin-main.component.ts
@@ -1,10 +1,67 @@
+import { Location } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/core/auth/auth.service';
 
 @Component({
   selector: 'app-admin-main',
   templateUrl: './admin-main.component.html',
-  styleUrls: ['./admin-main.component.scss']
+  styleUrls: ['./admin-main.component.scss'],
 })
 export class AdminMainComponent {
+  mobile = false;
+  isShowDropdown = false;
+  isShowMobileNav = false;
 
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private location: Location
+  ) {}
+
+  toggleMobile = () => {
+    this.mobile = !this.mobile;
+  };
+
+  toggleShowDropdown = () => {
+    this.isShowDropdown = !this.isShowDropdown;
+    this.isShowMobileNav = false;
+  };
+
+  openMobileNav = () => {
+    this.isShowMobileNav = true;
+    scroll(0, 0);
+  };
+
+  closeMobileNav = () => {
+    this.isShowMobileNav = false;
+  };
+
+  getLocation = () => {
+    const currentLocation = this.location.path();
+    const splitLocation = currentLocation.split('/');
+    const loc = splitLocation[splitLocation.length - 1];
+    if (loc == 'dashboard') {
+      return 'Dashboard';
+    } else if (loc == 'complaints') {
+      return 'Complaints';
+    } else if (loc == 'farming-tips') {
+      return 'Farming Tips';
+    } else {
+      return 'Profile';
+    }
+  };
+
+  profile = () => {
+    this.router.navigate(['/admin/profile']);
+    this.isShowDropdown = false;
+  };
+
+  logout = () => {
+    if (this.authService.isUserLoggedIn()) {
+      localStorage.removeItem('user');
+    }
+    this.router.navigate(['/']);
+    this.isShowDropdown = false;
+  };
 }
