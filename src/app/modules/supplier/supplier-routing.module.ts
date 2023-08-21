af61a112bd6bbfed38860fbfa9be0b6e3621e3c9
simonvillarin <simon.villarin@gmail.com>
2023-08-21 11:46:44 +0800

    sidebar

--- src/app/modules/supplier/supplier-routing.module.ts
+++ src/app/modules/supplier/supplier-routing.module.ts
@@ -3,6 +3,10 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { SupplierMainComponent } from './pages/supplier-main/supplier-main.component';
+import { AdvertisementComponent } from './pages/advertisement/advertisement.component';
+import { PaymentsComponent } from './pages/payments/payments.component';
+import { ReceivedComponent } from './pages/received/received.component';
+import { AdminProfileComponent } from './pages/admin-profile/admin-profile.component';
 
 const routes: Routes = [
   {
@@ -12,6 +16,22 @@ const routes: Routes = [
       {
         path: 'dashboard',
         component: DashboardComponent,
+      },
+      {
+        path: 'advertisement',
+        component: AdvertisementComponent,
+      },
+      {
+        path: 'payments',
+        component: PaymentsComponent,
+      },
+      {
+        path: 'received',
+        component: ReceivedComponent,
+      },
+      {
+        path: 'profile',
+        component: AdminProfileComponent,
       },
     ],
   },
--- src/app/modules/supplier/supplier.module.ts
+++ src/app/modules/supplier/supplier.module.ts
@@ -4,9 +4,13 @@ import { SupplierMainComponent } from './pages/supplier-main/supplier-main.compo
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { SupplierRoutingModule } from './supplier-routing.module';
+import { AdvertisementComponent } from './pages/advertisement/advertisement.component';
+import { PaymentsComponent } from './pages/payments/payments.component';
+import { ReceivedComponent } from './pages/received/received.component';
+import { AdminProfileComponent } from './pages/admin-profile/admin-profile.component';
 
 @NgModule({
-  declarations: [SupplierMainComponent, DashboardComponent],
+  declarations: [SupplierMainComponent, DashboardComponent, AdvertisementComponent, PaymentsComponent, ReceivedComponent, AdminProfileComponent],
   imports: [CommonModule, SharedModule, SupplierRoutingModule],
 })
 export class SupplierModule {}
