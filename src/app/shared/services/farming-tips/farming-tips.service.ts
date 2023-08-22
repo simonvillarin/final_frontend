a12f1c26a632c41b8a0ee214152c49f8e279b31d
AdrianRecortos <65625114+adrianrecortos@users.noreply.github.com>
2023-08-22 18:24:03 +0800

    farmingtips-fe

--- src/app/shared/services/farming-tips/farming-tips.service.ts
+++ src/app/shared/services/farming-tips/farming-tips.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FarmingTipsService {
+  baseUrl = 'http://localhost:8080/api/farming';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getAllFarmingTips = (): Observable<any[]> => {
+    return this.http.get<any[]>(`${this.baseUrl}/tips`);
+  };
+
+  addTip = (ad: any): Observable<any> => {
+    return this.http.post<any>(`${this.baseUrl}/tip`, ad);
+  };
+
+  updateTip = (id: number, ad: any) => {
+    return this.http.put<any>(`${this.baseUrl}/tip/${id}`, ad);
+  };
 }
