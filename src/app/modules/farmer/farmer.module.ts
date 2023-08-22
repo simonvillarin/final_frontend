7fe7bfc22fce97c375546cbb33da9da1667a840a
cbellecruz <ccruz5@77global.biz>
2023-08-22 18:50:09 +0800

    Farmer Module

--- src/app/modules/farmer/farmer.module.ts
+++ src/app/modules/farmer/farmer.module.ts
@@ -6,12 +6,14 @@ import { ComplaintsComponent } from './pages/complaints/complaints.component';
 import { SellProductComponent } from './pages/sell-product/sell-product.component';
 import { PaymentsComponent } from './pages/payments/payments.component';
 import { ScheduleComponent } from './pages/schedule/schedule.component';
-import { FarmerMainComponent } from './pages/farmer-main/farmer-main.component';
-import { SharedModule } from 'src/app/shared/shared.module';
-import { FarmerRoutingModule } from './farmer-routing.module';
 import { HistoryComponent } from './pages/history/history.component';
 import { AdvertisementComponent } from './pages/advertisement/advertisement.component';
 import { FarmerProfileComponent } from './pages/farmer-profile/farmer-profile.component';
+import { FarmerMainComponent } from './pages/farmer-main/farmer-main.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { FarmerRoutingModule } from './farmer-routing.module';
+import { CardModule } from 'primeng/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -26,6 +28,12 @@ import { FarmerProfileComponent } from './pages/farmer-profile/farmer-profile.co
     FarmerProfileComponent,
     FarmerMainComponent,
   ],
-  imports: [CommonModule, SharedModule, FarmerRoutingModule],
+  imports: [
+    CommonModule,
+    SharedModule,
+    FarmerRoutingModule,
+    CardModule,
+    MatIconModule
+  ],
 })
 export class FarmerModule {}
