be0f70dd7e86be1bab87a0c64e18cbc478942659
AdrianRecortos <65625114+adrianrecortos@users.noreply.github.com>
2023-08-22 14:38:20 +0800

    complaint

---
+++ src/app/shared/services/complaints/complaints.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ComplaintsService {
+  baseUrl = 'http://localhost:8080/api/farming';
+
+  constructor(private http: HttpClient) {}
+
+  getAllComplaints = (): Observable<any[]> => {
+    return this.http.get<any[]>(`${this.baseUrl}/complaints`);
+  };
+
+  addComplaint = (ad: any): Observable<any> => {
+    return this.http.post<any>(`${this.baseUrl}/complaint`, ad);
+  };
+
+  updateComplaint = (id: number, ad: any) => {
+    return this.http.put<any>(`${this.baseUrl}/complaint/${id}`, ad);
+  };
+}
