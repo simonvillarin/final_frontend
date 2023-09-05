058b095d075dbbd7dad996becc7c785e70ee37ab
Simon James Villarin <simon.villarin@gmail.com>
2023-09-05 17:03:09 +0800

    landing

--- src/app/modules/landing/components/contacts/contacts.component.ts
+++ src/app/modules/landing/components/contacts/contacts.component.ts
@@ -11,33 +11,4 @@ import {
   templateUrl: './contacts.component.html',
   styleUrls: ['./contacts.component.scss'],
 })
-export class ContactsComponent {
-  contactForm: FormGroup;
-
-  constructor(private fb: FormBuilder) {
-    this.contactForm = fb.group({
-      name: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      message: ['', Validators.required],
-    });
-  }
-
-  get name() {
-    return this.contactForm.get('name') as FormControl;
-  }
-
-  get email() {
-    return this.contactForm.get('email') as FormControl;
-  }
-
-  get message() {
-    return this.contactForm.get('message') as FormControl;
-  }
-
-  onSubmit = () => {
-    if (this.contactForm.valid) {
-    } else {
-      this.contactForm.markAllAsTouched();
-    }
-  };
-}
+export class ContactsComponent {}
